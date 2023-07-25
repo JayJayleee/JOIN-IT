19456f59a2721dd05b0f807bd261dcba97644c92
Alex-kim <ghg303@naver.com>
2023-07-26 00:17:31 +0900

    Feat: #S09P12B203-148 메인페이지 퍼블리싱 구현중, div 생성 완료, 버튼 추가 후 작업 완료 예정

--- Develop/FrontEnd/joinit/src/App.tsx
+++ Develop/FrontEnd/joinit/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import "./App.css";
 
-import Nav from './Components/Nav/Nav'
-import Home from './pages/Home'
+import Nav from "./Components/Nav/Nav";
+import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 
 import CareCreate from "./pages/T_care/careCreate";
@@ -10,14 +10,13 @@ import CareCreate from "./pages/T_care/careCreate";
 function App() {
   return (
     <div>
-      <div className='navdiv'>
+      <div className="navdiv">
         <Nav />
       </div>
       <Home />
-      <CareCreate />
+      {/* <CareCreate /> */}
     </div>
-  )
-      
+  );
 }
 
-export default App
+export default App;
