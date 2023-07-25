c9cdb3b857c8773c7704986f1ef6882486133055
Alex kim <ghg303@naver.com>
2023-07-25 16:02:39 +0900

    Feat:#S09P12B203-148 Navbar 퍼블리싱 구현

---
+++ Develop/FrontEnd/joinit/src/Components/Nav/Nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div>
+      <div className='Navbar'>
+        <img src="/Assets/Images/logo.png" alt='logo img' className='logoimg'/>
+        <img src="/Assets/Images/usericon.png" alt='usericon' className='usericon'/>
+      </div>
+    </div>
+  );
+}
+
+
+export default Nav;
