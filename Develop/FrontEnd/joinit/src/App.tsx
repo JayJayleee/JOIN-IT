94669402a616612f20ef55d8518e140cc4a8b8d5
JayJayleee <acd0825@naver.com>
2023-07-25 17:20:35 +0900

    Add: #S09P12B203-175 치료 페이지 생성 및 에디터기능 추가

--- Develop/FrontEnd/joinit/src/App.tsx
+++ Develop/FrontEnd/joinit/src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
+
+import CareCreate from "./pages/T_care/careCreate";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <div>
+      <CareCreate />
+    </div>
   );
 }
 
