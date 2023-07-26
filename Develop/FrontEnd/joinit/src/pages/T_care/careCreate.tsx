ded4c8c829f19cc45111c78558dbe998a0e06b91
JayJayleee <acd0825@naver.com>
2023-07-26 17:53:55 +0900

    Fix: S09P12B203-175 환자 치료 등록 페이지 컴포넌트 구성 완료

--- Develop/FrontEnd/joinit/src/pages/T_care/careCreate.tsx
+++ Develop/FrontEnd/joinit/src/pages/T_care/careCreate.tsx
@@ -16,29 +16,33 @@ function CareCreate() {
         <div className="patientInfo1">
           <div className="patientName widthForInput">
             <h4>환자 이름</h4>
-            <input className="inputEditfor2" type="text" />
+            <input className="input" type="text" />
           </div>
+          <div className="whiteSpace"></div>
           <div className="patientPain widthForInput">
             <h4>환부</h4>
-            <select className="inputEditfor2 rightside" name="bodypart" id="">
-              <option value="choose">옵션을 선택해주세요.</option>
+            <select className="select" name="bodypart" id="">
+              <option value="choose">please</option>
               <option value="neck">목</option>
               <option value="back">등</option>
             </select>
           </div>
         </div>
-
-        <div className="patientInfo2 widthForInput">
+        <div className="widthForInput">
           <h4>환자 연락처</h4>
-          <input className="inputEditfor1" type="text" />
+          <input className="input" type="text" />
           <h4>진료 시작일</h4>
-          <input className="inputEditfor1" type="text" />
+          <input className="input" type="date" />
           <h4>치료 요약</h4>
-          <input className="inputEditfor1" type="text" />
+          <input className="input" type="text" />
           <h4>사고 경위</h4>
-          <input className="inputEditfor1" type="text" />
+          <input className="input" type="text" />
           <h4>특이 사항</h4>
           <textarea className="textareEdit" />
+          <div className="button-2">
+            <div className="eff-2"></div>
+            <a href="#"> Touch me </a>
+          </div>
         </div>
       </div>
     </div>
